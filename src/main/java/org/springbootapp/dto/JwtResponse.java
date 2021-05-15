bbf227b1abbc1711a473e8df5b2445e75e11f38d
daotiensyyyy <sydao1579@gmail.com>
2021-05-15 21:44:58 +0700

    jwt response

--- src/main/java/org/springbootapp/dto/JwtResponse.java
+++ src/main/java/org/springbootapp/dto/JwtResponse.java
@@ -1,13 +1,6 @@
 package org.springbootapp.dto;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springbootapp.entity.User;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import lombok.Getter;
 import lombok.Setter;
